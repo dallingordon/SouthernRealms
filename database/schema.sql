f7523f22e9d5b1fe883f21559e4071205e39b163
dallingordon <dallingordon@gmail.com>
2024-04-23 21:05:59 -0400

    shufflin

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,18 @@
-CREATE TABLE cards (
+CREATE TABLE IF NOT EXISTS cards (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     filename TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS deck (
+    deckid INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS decksetup (
+    decksetupid INTEGER PRIMARY KEY,
+    cardid INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    deckid INTEGER NOT NULL,
+    FOREIGN KEY (cardid) REFERENCES card(cardid),
+    FOREIGN KEY (deckid) REFERENCES deck(deckid)
 );
